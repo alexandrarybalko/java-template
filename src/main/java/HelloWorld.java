2eb7157a8d555d54637ef4d792cf3eeeb2edb072
Alexandra Rybalko <alexandrarybalko21@gmail.com>
2020-09-10 11:23:19 +0300

    test commit

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,8 +4,8 @@
  */
 // назавние главного класса, обычно совпадает с названием программы
 public class HelloWorld {
-  public static void main (String args[]) {
-    String helloMessage = "Hello World!";
+  public static void main (String[] args) {
+    String helloMessage = "Hello World!!!";
     System.out.println(helloMessage);
   }
 }
